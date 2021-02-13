649fd25cde1c8e24e13e691df6720fe740ad4bd1
DuvanDu <duque.duvan@uces.edu.co>
2021-02-13 16:54:51 -0500

    subiendo taller 1

---
+++ Talleres/Taller.py
@@ -0,0 +1,21 @@
+numeroX = 8
+numeroY = 22
+print ("#"*15, "¿X Mayor que Y?", "#"*15)
+isMayorNumero = numeroX > numeroY
+print (isMayorNumero)
+print ("#"*15, "¿X menor que Y?", "#"*15)
+isMenorNumero = numeroX < numeroY
+print (isMenorNumero)
+print ("#"*15, "¿X igual a Y?", "#"*15)
+isIgualNumero = numeroX == numeroY
+print (isIgualNumero)
+sumar = numeroX + numeroY
+print ("el resultado de la suma", sumar)
+resta = numeroX - numeroY
+print ("el resultado de la resta", resta)
+multiplicacion = numeroX * numeroY
+print ("el resultado de la multiplicacion", multiplicacion)
+dividir = numeroX / numeroY
+print ("el resultado de la division", dividir)
+elevar = numeroX ** numeroY
+print ("el resultado del exponente", elevar)
