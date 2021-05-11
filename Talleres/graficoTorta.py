132619333796cca8a089cde15fc73edb56c585f7
DuvanDu <duque.duvan@uces.edu.co>
2021-05-11 01:38:55 -0500

    .

--- Talleres/graficoTorta.py
+++ Talleres/graficoTorta.py
@@ -3,15 +3,13 @@ pieExplode = [0.2,0,0,0]
 acumulador = 0
 porcentajeHabitantes = [5.10,4.42,1.81,0.99]
 pieLabels = ['Medellin', 'Cali', 'Cartagena', 'SantaMarta',]
-def etiquetarElementosPorcentuales(porcentajeHabitantes, labels, indicador= ' ->'):
 
-  for i in range (len(labels)):
-    pieLabels[i] += str(porcentajeHabitantes) +'%'
+for i in range (len(pieLabels)):
+  pieLabels[i] += str(porcentajeHabitantes) +'%'
 
 plt.pie(porcentajeHabitantes,labels=pieLabels, 
         explode=pieExplode, 
         shadow= True, 
-        counterclock = True, 
         startangle= 45)
 #####################
 plt.title('Porcentaje de la poblacion 2018')
